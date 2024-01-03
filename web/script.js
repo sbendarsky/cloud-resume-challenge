4ee851c68a6fec29ea792bffc57efc2473ba2236
Stav Bendarsky <sbendarsky@gmail.com>
2024-01-03 09:52:45 +0200

    Update backend.yml and script.js

--- web/script.js
+++ web/script.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://h3pniu4x6kr2dv2u7mhgyad3se0ovypn.lambda-url.us-east-1.on.aws/"
+        "https://rrch6yoqsmpsln5qbissoy3vk40jnglm.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `Visitors: ${data}`;
