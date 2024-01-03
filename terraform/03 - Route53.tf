2099203a5223452934a114bb3ad1f0e12ddcd366
Stav Bendarsky <sbendarsky@gmail.com>
2024-01-03 09:34:41 +0200

    Add CI/CD workflow for Lambda, create Route53 record, and configure Lambda function

---
+++ terraform/03 - Route53.tf
@@ -0,0 +1,12 @@
+#Create Route53 Record
+resource "aws_route53_record" "cloudfront_record" {
+  zone_id = "Z0812785FWYP51NI40L5"  
+  name    = "terraform"     
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
