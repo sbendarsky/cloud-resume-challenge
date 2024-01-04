ccfde94f7ea6730bddca2e1f55300d375815c838
Stav Bendarsky <sbendarsky@gmail.com>
2024-01-04 23:52:08 +0200

    Update test_lambda_function.py

--- tests/test_lambda_function.py
+++ tests/test_lambda_function.py
@@ -2,9 +2,11 @@ import json
 import requests
 import pytest
 
+# Function to fetch Lambda URL from the provided API
 def get_lambda_url():
     api_url = "https://resume.sbendarsky.me/api.txt"
     response = requests.get(api_url)
+
     if response.status_code == 200:
         return response.text.strip()
     else:
@@ -15,15 +17,23 @@ def lambda_url():
     return get_lambda_url()
 
 def test_lambda_function(lambda_url):
-    event = {}
+    event = {"key": "value"}
+
+    # Send a POST request to the Lambda function
     response = requests.post(lambda_url, data=json.dumps(event))
+
+    # Check if the request was successful (status code 200)
     assert response.status_code == 200
 
     try:
         result = response.json()
+        if isinstance(result, int):
+            result = {"views": result}
         assert "views" in result
         assert isinstance(result["views"], int)
+
     except json.JSONDecodeError:
+        # Handle the case where the response is not a valid JSON
         pytest.fail("Response is not a valid JSON")
 
 if __name__ == "__main__":
